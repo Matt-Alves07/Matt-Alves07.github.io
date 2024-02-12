ba54a5a50ebadd5db38059ccfbdcfad62326316b
Matheus Alves <mathalves07.winstore@outlook.com>
2024-02-12 14:14:39 -0300

    Created the shared folder, and the toolbar component inside it, and also included the angular materia reference
    resolves #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuToolbarComponent } from './shared/components/menu-toolbar/menu-toolbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenuToolbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

e69fc9230d1c213c26a1131878b68e8c5ea8cbf8
Matheus da Silva Alves <mathalves07.winstore@outlook.com>
2026-03-25 20:36:58 -0300

    Deletado o projeto antigo, e criado um novo a partir do zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css';
-
-import { Outlet } from "react-router-dom";
-import ToolbarComponent from './components/Toolbar/Toolbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className='App'>
-      <ToolbarComponent />
-
-      <div className="outlet-container">
-        <Outlet />
+    <Router>
+      <div className="layout-container">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </div>
-    </div>
+    </Router>
   );
 }
 
